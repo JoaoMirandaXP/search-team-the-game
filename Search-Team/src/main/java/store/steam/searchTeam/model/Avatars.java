02710ffbfdc53ff9227ea55bbebc00b037b24894
JoaoMirandaXP <joaozinhocavalcante.jovc@gmail.com>
2018-10-01 00:44:18 -0300

    Comentarios doq fazer depois

--- Search-Team/src/main/java/store/steam/searchTeam/model/Avatars.java
+++ Search-Team/src/main/java/store/steam/searchTeam/model/Avatars.java
@@ -1,21 +1,39 @@
 package store.steam.searchTeam.model;
 //Classe mãe , meio q ela define oque todo personagem pode fazer no geral
 public class Avatars {
+
+	private int SCREEN_WIDTH=123;
+	private int SCREEN_HEIGHT=234;
+	
 	private int positionX;
 	private int positionY;
 	
 	public void moveUP() {
+		/*
+		 * fazer laço while para o personagem continuar subindo(animação) e depois cair
+		 * e tambem fazer uns boolean loko p juntar com os ataques
+		 */
 		this.positionY++;
 	}
 
 	public void moveDown() {
+		/*
+		 * modificar para a transição do personagem agachado  
+	 	 * e colocar uma condição booleana aqui para combinar com os ataques
+		 */
 		this.positionY--;
 	}
+	
 	public void moveLeft() {
-		this.positionX--;
+		if(this.positionX>0) {
+			this.positionX--;
+		}
 	}
+	
 	public void moveRight() {
-		this.positionX++;
+		if(this.positionX<SCREEN_WIDTH) {
+			this.positionX++;
+		}
 	}
 
 }
