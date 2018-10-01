973876b46d87992c2a20ef5b7e6eb68cb195bbbd
JoaoMirandaXP <joaozinhocavalcante.jovc@gmail.com>
2018-09-30 23:37:49 -0300

    Upload inicial do Projeto

---
+++ Search-Team/src/main/java/store/steam/searchTeam/App.java
@@ -0,0 +1,13 @@
+package store.steam.searchTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
