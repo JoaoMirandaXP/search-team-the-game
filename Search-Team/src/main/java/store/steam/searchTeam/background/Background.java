3999b5b5b450acf3cecfeccd95a561f92fd76630
JoaoMirandaXP <joaozinhocavalcante.jovc@gmail.com>
2018-10-02 23:43:53 -0300

    Inserindo a Classe de background e os testes de sprites

---
+++ Search-Team/src/main/java/store/steam/searchTeam/background/Background.java
@@ -0,0 +1,13 @@
+package store.steam.searchTeam.background;
+
+public class Background {
+
+	/*
+	 * implementar o tamanho real do cenario e ajustar para o tamanho da tela para acontecer um scroll
+	 * talvez alguma interação com o cenário e ligação do cenário e
+	 * mudança de tela dependendo do movimento realizado com sucesso
+	 * 
+	 */
+	
+	
+}
