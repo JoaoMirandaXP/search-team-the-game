8ba1e978fa14261bdda796bb5673d380a2b27547
JoaoMirandaXP <joaozinhocavalcante.jovc@gmail.com>
2018-10-07 00:52:21 -0300

    problema dos ifs

--- Search-Team/src/main/java/store/steam/searchTeam/background/Background.java
+++ Search-Team/src/main/java/store/steam/searchTeam/background/Background.java
@@ -2,6 +2,9 @@ package store.steam.searchTeam.background;
 
 public class Background {
 
+	public final static int  SCREEN_WIDTH=260;
+	public final static int SCREEN_HEIGHT=140;	
+	
 	/*
 	 * implementar o tamanho real do cenario e ajustar para o tamanho da tela para acontecer um scroll
 	 * talvez alguma interação com o cenário e ligação do cenário e
